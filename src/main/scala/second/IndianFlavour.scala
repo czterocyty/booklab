5a333bc55bd0643c13d5ba300e0a93d07a8b8c75
Adam Domanski <adomanski@gmail.com>
2018-02-12 14:51:53 +0100

    after meeting

--- src/main/scala/second/IndianFlavour.scala
+++ src/main/scala/second/IndianFlavour.scala
@@ -3,7 +3,16 @@ package second
 object IndianFlavour {
 
   def curry[A, B, C](f: (A, B) => C): A => (B => C) = {
-    (a: A) => (b: B) => f(a, b)
+    // expanded form
+    def a2f(a: A): B => C = {
+      def b2c(b: B): C = {
+        f(a, b)
+      }
+      b2c
+    }
+    a2f
+
+    // (a: A) => (b: B) => f(a, b)
   }
 
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
