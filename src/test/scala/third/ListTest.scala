bfbe765733ea71663fa123fb972697936b9d4059
Adam Domanski <adomanski@gmail.com>
2018-02-16 09:33:49 +0100

    test from FunSpec to FunSuite

--- src/test/scala/third/ListTest.scala
+++ src/test/scala/third/ListTest.scala
@@ -1,10 +1,10 @@
 package third
 
-import org.scalatest.FunSuite
+import org.scalatest.FunSpec
 
-class ListTest extends FunSuite {
+class ListTest extends FunSpec {
 
-  def matchTest(list: List[Int]): Int = {
+  def matchit(list: List[Int]): Int = {
     list match {
       case Cons(x, Cons(2, Cons(4, _))) => x
       case Nil => 42
@@ -14,64 +14,72 @@ class ListTest extends FunSuite {
     }
   }
 
-  test("output of match - default") {
-    assert(matchTest(List(1, 2, 3, 4, 5)) == 3)
+  describe("matchit") {
+    it("output of match - default") {
+      assert(matchit(List(1, 2, 3, 4, 5)) == 3)
+    }
+
+    it("output of match - Nil") {
+      assert(matchit(Nil) == 42)
+    }
+
+    it("output of match - head") {
+      assert(matchit(List(1, 2, 4)) == 1)
+    }
+
+    it("output of match - sum folded") {
+      assert(matchit(List(1, 2, 3)) == 6)
+      assert(matchit(List(1)) == 1)
+    }
+
+    it("output of match - last case") {
+      // seems to be dead code
+    }
   }
 
-  test("output of match - Nil") {
-    assert(matchTest(Nil) == 42)
+  describe("tail") {
+    it("tail - empty") {
+      assert(List.tail(Nil) == Nil)
+    }
+
+    it("tail - one element") {
+      assert(List.tail(List("A")) == Nil)
+    }
+
+    it("tail - two elements") {
+      assert(List.tail(List("A", "B")) == List("B"))
+    }
   }
 
-  test("output of match - head") {
-    assert(matchTest(List(1, 2, 4)) == 1)
+  describe("setHead") {
+    it("setHead for empty") {
+      assert(List.setHead(Nil, "a") == Nil)
+    }
+
+    it("setHead for one element") {
+      assert(List.setHead(List("A"), "B") == List("B"))
+    }
+
+    it("setHead for more elements") {
+      assert(List.setHead(List("A", "B"), "C") == List("C", "B"))
+    }
   }
 
-  test("output of match - sum folded") {
-    assert(matchTest(List(1, 2, 3)) == 6)
-    assert(matchTest(List(1)) == 1)
-  }
+  describe("drop") {
+    it("drop - empty") {
+      assert(List.drop(Nil, 3) == Nil)
+    }
 
-  test("output of match - last case") {
-    // seems to be dead code
-  }
+    it("drop one-element") {
+      assert(List.drop(List("A"), 1) == Nil)
+    }
 
-  test("tail - empty") {
-    assert(List.tail(Nil) == Nil)
-  }
+    it("drop nothing when 0") {
+      assert(List.drop(List("A"), 0) == List("A"))
+    }
 
-  test("tail - one element") {
-    assert(List.tail(List("A")) == Nil)
-  }
-
-  test("tail - two elements") {
-    assert(List.tail(List("A", "B")) == List("B"))
-  }
-
-  test("setHead for empty") {
-    assert(List.setHead(Nil, "a") == Nil)
-  }
-
-  test("setHead for one element") {
-    assert(List.setHead(List("A"), "B") == List("B"))
-  }
-
-  test("setHead for more elements") {
-    assert(List.setHead(List("A", "B"), "C") == List("C", "B"))
-  }
-
-  test("drop - empty") {
-    assert(List.drop(Nil, 3) == Nil)
-  }
-
-  test("drop one-element") {
-    assert(List.drop(List("A"), 1) == Nil)
-  }
-
-  test("drop nothing when 0") {
-    assert(List.drop(List("A"), 0) == List("A"))
-  }
-
-  test("drop twice") {
-    assert(List.drop(List(1, 2, 3), 2) == List(3))
+    it("drop twice") {
+      assert(List.drop(List(1, 2, 3), 2) == List(3))
+    }
   }
 }
